08a93fe59083be459c11140f6310f6a79708258a
WNN6231 <3429236231@qq.com>
2026-03-08 17:59:33 +0800

    增加tailwind配置项

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // 确保这里的路径覆盖了你所有的组件和页面
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // 如果有 src 目录
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      // 你可以这里自定义文章页面的配色
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: '100ch', // 限制文章最大宽度，提升阅读感
-          },
-        },
+      colors: {
+        background: "#18181b",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'), // 这一行最关键
+    require('@tailwindcss/typography'),
   ],
 };
-
 export default config;
