b9569044c091d96822460222827c031e01329b3d
WNN6231 <3429236231@qq.com>
2026-03-21 16:01:27 +0800

    fix(deploy): 手动创建 wrangler 配置文件并移除错误绑定

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
+    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'n.sinaimg.cn',
       }
     ]
-  }
+  },
 };
 
 export default nextConfig;
